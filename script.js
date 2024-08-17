0b0f49fb302eb56267b698164fac89a3a33b2ba6
karamo10 <150404491+karamo10@users.noreply.github.com>
2024-08-17 15:21:41 -0700

    css change

--- script.js
+++ script.js
@@ -15,13 +15,29 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     }
+    // animated skill bar 
+    const skillLevels = document.querySelectorAll('.skill-level');
+
+    skillLevels.forEach(level => {
+        const flows = level.getAttribute('data-level');
+        setTimeout(() => {
+            level.style.width = flows;
+        }, 500);
+    });
 });
 
-const myFunction = ()=> {
+function myFunction() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
-      x.style.display = "none";
+        x.style.display = "none";
     } else {
-      x.style.display = "block";
+        x.style.display = "block";
     }
-  }
+}
+// prealoder function
+window.addEventListener('load', function(){
+    var preload = document.querySelector('.preloader');
+    setTimeout( function(){
+        preload.classList.add('hide-preloader');
+    }, 6000);
+});
